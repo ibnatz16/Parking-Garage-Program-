205b5d27bcb524a1d8f9dc7531b05f401715b39e
nehasharma2998 <nehasharma29@outlook.com>
2017-11-28 15:15:56 -0500

    Added More
    Tell me what to do and not do guys

--- ParkingGarage(Neha)/ParkingGarage.cpp
+++ ParkingGarage(Neha)/ParkingGarage.cpp
@@ -4,28 +4,11 @@
 using namespace std;
 #include "parkingGarage.h"
 
-void arrivingGreeting() {
 
-	cout << "--\nThanks for parking your garage in the Zanach Parking Garage. \nWe promise your car is in pretty good hands for the most part!" << endl;
-	cout << "\nOur Regular Rates:" << endl;
-		cout << "\t<1 hour: $10" << endl;
-		cout << "\t1 - 3 hour(s): $12.50" << endl;
-		cout << "\t3 - 6 hours: $17.50" << endl;
-		cout << "\t6 - 12 hours: $24.50" << endl;
-		cout << "\t12 - 24 hours: $34.50" << endl;
-
-	cout << "\nOur VIP Rates:" << endl;
-		cout << "\t<1 hour: $20" << endl;
-		cout << "\t1 - 3 hours: $22.50" << endl;
-		cout << "\t3 - 6 hours: $27.50" << endl;
-		cout << "\t6 - 12 hours: $34.50" << endl;
-		cout << "\t12 - 24 hours: $44.50" << endl;
-
-}
 
 
 int main() {
-	int menuChoice;
+	int menuChoice, VIPchoice;
 
 
 	cout << "Welcome to the Zanach Parking Garage!!" << endl;
@@ -38,11 +21,57 @@ int main() {
 	cout << "Choice: ";
 	cin >> menuChoice;
 
+	
+
+
 	switch (menuChoice) {
 	case 1: 
-		arrivingGreeting();
+
+		cout << "--\nThanks for parking your garage in the Zanach Parking Garage. \nWe promise your car is in pretty good hands for the most part!" << endl;
+		cout << "\nOur Regular Rates:" << endl;
+		cout << "\t<1 hour: $10" << endl;
+		cout << "\t1 - 3 hour(s): $12.50" << endl;
+		cout << "\t3 - 6 hours: $17.50" << endl;
+		cout << "\t6 - 12 hours: $24.50" << endl;
+		cout << "\t12 - 24 hours: $34.50" << endl;
+
+		cout << "\nOur VIP Rates:" << endl;
+		cout << "\t<1 hour: $20" << endl;
+		cout << "\t1 - 3 hours: $22.50" << endl;
+		cout << "\t3 - 6 hours: $27.50" << endl;
+		cout << "\t6 - 12 hours: $34.50" << endl;
+		cout << "\t12 - 24 hours: $44.50" << endl;
+
+		bool VIP = false;
+
+		cout << "Would you like to park regular or VIP?" << endl;
+
+		cout << "1. Regular" << endl;
+		cout << "2. VIP" << endl;
+		cout << "3. JK I don't want to park you guys are too expensive" << endl;
+		cout << "Choice: ";
+
+		cin >> VIPchoice;
+
+		switch (VIPchoice) {
+		case 1: 
+			VIP = false; 
+			//car.isVIP = false or whatever
+			break;
+		case 2: 
+			VIP = true; 
+			//car.isVIP = true or whatever
+			break;
+		case 3:
+			cout << "goodbye, thanks for nothing";
+			//return 0;
+			break;
+		}//end of VIP switch statement
+
 		break;
-	}
+
+
+	}//end of big switch statement
 
 }
 
