c5c7d6e909651d1da786ee9943a2a235d9e6e319
nehasharma2998 <nehasharma29@outlook.com>
2017-11-30 12:59:36 -0500

    Compiles.
    Havent; checked the getters and setters but edited everything that was causing problems. Going to work on vectors next.

--- ParkingGarage(Neha)/ParkingGarage.cpp
+++ ParkingGarage(Neha)/ParkingGarage.cpp
@@ -1,14 +1,125 @@
 #include <iostream>
-
+#include <string>
+#include <string.h>
+#include <vector>
 
 using namespace std;
 #include "parkingGarage.h"
 
 
 
+//constructor for car object
+//initiates all variables to null or zero
+Car::Car() {
+	color = "";
+	make = "";
+	model = "";
+	lpNum = "";
+	floorNum = 0;
+	spaceNum = 0;
+	timePeriod = 0;
+	isVIP = false;
+}
+//Setters
+void Car::set_color(string c)
+{
+	color = c;
+}
+
+void Car::set_make(string m)
+{
+	make = m;
+}
+
+void Car::set_model(string m)
+{
+	model = m;
+}
+
+void Car::set_lpNum(string lp)
+{
+	lpNum = lp;
+}
+
+void Car::set_floorNum(int fn)
+{
+	floorNum = fn;
+}
+
+void Car::set_spaceNum(int sn)
+{
+	spaceNum = sn;
+}
+
+void Car::set_timePeriod(int tp)
+{
+	timePeriod = tp;
+}
+
+void Car::set_moneyOwed(int m)
+{
+	moneyOwed = m;
+}
+
+void Car::set_isVIP(bool v)
+{
+	isVIP = v;
+}
+
+
+//Getters
+string Car::get_color()
+{
+	return color;
+}
+
+string Car::get_make()
+{
+	return make;
+}
+
+string Car::get_model()
+{
+	return model;
+}
+
+string Car::get_lpNum()
+{
+	return lpNum;
+}
+
+int Car::get_floorNum()
+{
+	return floorNum;
+}
+
+int Car::get_spaceNum()
+{
+	return spaceNum;
+}
+
+int Car::get_timePeriod()
+{
+	return timePeriod;
+}
+
+int Car::get_moneyOwed()
+{
+	return moneyOwed;
+}
+
+bool Car::get_isVIP()
+{
+	return isVIP;
+}
 
 int main() {
-	int mainMenuChoice, VIPchoice, timeChoice;
+
+	
+	int mainMenuChoice = 0, VIPchoice = 0, timeChoice = 0; //neha
+	int floorTemp, spaceTemp, paymentChoice; //austin
+	string makeInput, modelInput, colorInput, lpInput, lpTemp; //austin
+	double fee; //austin
 
 
 	cout << "Welcome to the Zanach Parking Garage!!" << endl;
@@ -27,8 +138,7 @@ int main() {
 		cin >> mainMenuChoice;
 	}
 
-	
-
+	Car car;
 
 	switch (mainMenuChoice) {
 		//parking the car, car is arriving 
@@ -36,14 +146,17 @@ int main() {
 		//declaring a new car object
 		
 
-		cout << "--\nThanks for parking your garage in the Zanach Parking Garage. \nWe promise your car is in pretty good hands for the most part!" << endl;
-		cout << "\nOur Regular Rates:" << endl;
+		cout << "--\nThanks for parking your garage in the Zanach Parking Garage."
+			<< "\nWe promise your car is in pretty good hands for the most part!" << endl;
+
+		//prints out regular rates
 		cout << "\t<1 hour: $10" << endl;
 		cout << "\t1 - 3 hour(s): $12.50" << endl;
 		cout << "\t3 - 6 hours: $17.50" << endl;
 		cout << "\t6 - 12 hours: $24.50" << endl;
 		cout << "\t12 - 24 hours: $34.50" << endl;
 
+		//prints out VIP rates
 		cout << "\nOur VIP Rates:" << endl;
 		cout << "\t<1 hour: $20" << endl;
 		cout << "\t1 - 3 hours: $22.50" << endl;
@@ -52,9 +165,7 @@ int main() {
 		cout << "\t12 - 24 hours: $44.50" << endl;
 
 
-		//asking for VIP
-
-		//bool VIP = false;
+		//asks to pick regular or VIP rates
 
 		cout << "Would you like to park regular or VIP?" << endl;
 
@@ -71,15 +182,18 @@ int main() {
 			cin >> VIPchoice;
 		}
 
+		
 
 			switch (VIPchoice) {
+				//user chooses regular rates
 			case 1: 
 				//VIP = false; 
 				//car.isVIP = false or whatever
 				break;
 			case 2: 
-				//VIP = true; 
-				//car.isVIP = true or whatever
+				//isVIP is set to true
+				//bool temp = true;
+				car.set_isVIP(true);
 				break;
 			case 3:
 				cout << "goodbye, thanks for nothing";
@@ -93,6 +207,8 @@ int main() {
 		//if car.isVIP cout "you are a VIP!"
 		//else if they are not
 
+
+			//asks which time period they will be paying for 
 		cout << "How long would you like to stay?" << endl;
 		cout << "1. <1 hour" << endl;
 		cout << "2. 1 - 3 hours" << endl;
@@ -112,7 +228,105 @@ int main() {
 
 		//if statements to add money to the car object for the time choice 
 
+		switch (timeChoice) {
+		case 1:
+			car.set_timePeriod(1);
+			break;
+		case 2:
+			car.set_timePeriod(2);
+			break;
+		case 3:
+			car.set_timePeriod(3);
+			break;
+		case 4:
+			car.set_timePeriod(4);
+			break;
+		case 5:
+			car.set_timePeriod(5);
+			break;
+		default:
+			cout << "That wasn't an option. Goodbye";
+			break;
+		}//end of time period switch statement
 
+		 //determines the fee that will be paid upon exit
+		//checks is user is VIP or regular and then sets moneyOwed
+		if (car.get_isVIP() == false) {
+			if (car.get_timePeriod() == 1) {
+				fee = 10;
+				car.set_moneyOwed(fee);
+			}
+			if (car.get_timePeriod() == 2) {
+				fee = 12.5;
+				car.set_moneyOwed(fee);
+			}
+			if (car.get_timePeriod() == 3) {
+				fee = 17.5;
+				car.set_moneyOwed(fee);
+			}
+			if (car.get_timePeriod() == 4) {
+				fee = 24.5;
+				car.set_moneyOwed(fee);
+			}
+			if (car.get_timePeriod() == 5) {
+				fee = 34.5;
+				car.set_moneyOwed(fee);
+			}
+
+		}
+		else {
+			if (car.get_timePeriod() == 1) {
+				fee = 20;
+				car.set_moneyOwed(fee);
+			}
+			if (car.get_timePeriod() == 2) {
+				fee = 22.5;
+				car.set_moneyOwed(fee);
+			}
+			if (car.get_timePeriod() == 3) {
+				fee = 27.5;
+				car.set_moneyOwed(fee);
+			}
+			if (car.get_timePeriod() == 4) {
+				fee = 34.5;
+				car.set_moneyOwed(fee);
+			}
+			if (car.get_timePeriod() == 5) {
+				fee = 44.5;
+				car.set_moneyOwed(fee);
+			}
+		}
+
+		//asks for make, model, color, and license plate of vehicle
+
+		cout << "Please enter your vehicle's make:" << endl;
+		cin >> makeInput;
+		car.set_make(makeInput);
+		cout << "Please enter your vehicle's model:" << endl;
+		cin >> modelInput;
+		car.set_model(modelInput);
+		cout << "Please enter your vehicle's color:" << endl;
+		cin >> colorInput;
+		car.set_color(colorInput);
+		cout << "Please enter a 6 character license plate number:" << endl;
+		cin >> lpInput;
+		//checks that the license plate is exactly 6 characters long
+		while (lpInput.length() != 6) {
+			cout << "Please enter a 6 character license plate number:";
+				cin >> lpInput;
+		}
+		//checks that each character in the string is either a number or a letter
+		for (int i = 0; i<6; i++) {
+			if (isalnum(lpInput.at(i)) == false) {
+				cout << "You entered an invalid character."<< "Please enter a license plate with only numbers and letters." << endl;
+				cin >> lpInput;
+			}
+		}
+		car.set_lpNum(lpInput);
+
+		//now the car needs to be assigned to a spot
+
+		//now the car need to be assigned to a spot
 
 
 
@@ -122,8 +336,60 @@ int main() {
 
 
 		break; //end of the case 1, car arriving in the parkinglot
-	case 2: break;
-	case 3: break;
+		case 2: 
+		
+		
+		
+		
+		//checkout and pay
+			//check out Zahin's code
+
+		cout << "Thank you for parking with Zanach!" << endl;
+		cout << "Please enter your floor number:" << endl;
+		cin >> floorTemp;
+		cout << "Please enter your space number:" << endl;
+		cin >> spaceTemp;
+
+		//code that finds which car object is at that space
+		//and then calls the get_moneyOwed() method for that object
+		//amount of money owed is then displayed to the user
+
+		cout << "You owe $" << car.get_moneyOwed() << "." << endl;
+
+		cout << "Would you like to pay with/n1. Cash/n2. Card" << endl;
+		cin >> paymentChoice;
+
+		if (paymentChoice == 1) {
+			cout << "Please take your change and have a great day!" << endl;
+		}
+		if (paymentChoice == 2) {
+			cout << "Please insert your card.n/Authorizing...n/"
+				<< "Please take your receipt and have a great day!" << endl;
+		}
+
+		
+		
+		
+		
+		
+		
+		break;
+	case 3: 
+		
+		cout << "Please enter your license plate number:" << endl;
+		cin >> lpTemp;
+
+		//locates car using unique 6 character license plate number
+		//need to loop through vector to find car object with given license plate number
+		//get floorNum and spaceNum when correct object is found and display them to the user
+
+		cout << "Your" << car.get_color() << car.get_make() << car.get_model() <<
+			" is located on floor " << car.get_floorNum() << " in space " << car.get_spaceNum() << "." << endl;
+
+		
+		
+		
+		break;
 	default:
 		cout << "That wasn't an option. Goodbye";
 		break;
--- ParkingGarage(Neha)/ParkingGarage.h
+++ ParkingGarage(Neha)/ParkingGarage.h
@@ -1,13 +1,59 @@
-#pragma once
+#include <string>
+#include <string.h>
 
-class Garage {
+using namespace std;
 
-	//void arrivingGreeting();
+class Car {
+
+public:
+	Car();//defining the object - Neha
+		  //declarations of setter methods for car variables
+	/*
+	void set_color(string c);
+	void set_make(string m);
+	void set_model(string m);
+	void set_lpNum(string lpN);
+	void set_floorNum(int fn);
+	void set_spaceNum(int sn);
+	void set_timePeriod(int tp);
+	void set_moneyOwed(int m);
+	void set_isVIP(bool v*/
 
 
+	//declarations of setter methods for car variables
+	void set_color(string c);
+	void set_make(string m);
+	void set_model(string m);
+	void set_lpNum(string lpN);
+	void set_floorNum(int fn);
+	void set_spaceNum(int sn);
+	void set_timePeriod(int tp);
+	void set_moneyOwed(int m);
+	void set_isVIP(bool v);
+	
+	//declarations of getter methods for car variables
+	string get_color();
+	string get_make();
+	string get_model();
+	string get_lpNum();
+	int get_floorNum();
+	int get_spaceNum();
+	int get_timePeriod();
+	int get_moneyOwed();
+	bool get_isVIP();
 
 
+private:
+	//All car variable declarations
+	string color;
+	string make;
+	string model;
+	string lpNum;
+	int floorNum;
+	int spaceNum;
+	int timePeriod;
+	int moneyOwed;
+	bool isVIP;
 
 
 };
-
