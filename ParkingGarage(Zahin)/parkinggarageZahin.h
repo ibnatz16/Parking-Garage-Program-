2186ce2689cfda576dcff546f1932d34bf9ae15f
Zahin Ibnat <zahinibnat@yahoo.com>
2017-11-30 14:22:30 -0500

    Put out wrong header. Here is correct one

--- ParkingGarage(Zahin)/parkinggarageZahin.h
+++ ParkingGarage(Zahin)/parkinggarageZahin.h
@@ -1,35 +1,59 @@
-#ifndef PA3_H
-#define PA3_H
-#include <iostream>
+/*
+ * parkinggarageZahin.h
+ *
+ *  Created on: Nov 28, 2017
+ *      Author: ibnatz16
+ */
+
+#ifndef PARKINGGARAGEZAHIN_H_
+#define PARKINGGARAGEZAHIN_H_
+
 #include <string>
-#include "stdio.h"
 
 using namespace std;
 
-class pa3{
-private:
-    int size;
-    //Struct used to make node
-    typedef struct node {
-        
-        string data;
-        node *next;
-        
-    }* nodePtr;
-    
-    nodePtr head, temp, now;
+class Car{
     
 public:
-    pa3();
-    void push(string add);
-    string pop();
-    bool empty();
-    bool notFree(string data);
-    int stackSize();
-    void print();
-    bool isTrue();
+    Car();
+    //declarations of setter methods for car variables
+    void set_color(string c);
+    void set_make(string m);
+    void set_model(string m);
+    void set_lpNum(string lpN);
+    void set_floorNum(int fn);
+    void set_spaceNum(int sn);
+    void set_timePeriod(int tp);
+    void set_moneyOwed(double m);
+    void set_isOccupied(bool o);
+    void set_isVIP(bool v);
+    
+    //declarations of getter methods for car variables
+    string get_color();
+    string get_make();
+    string get_model();
+    string get_lpNum();
+    int get_floorNum();
+    int get_spaceNum();
+    int get_timePeriod();
+    double get_moneyOwed();
+    bool get_isOccupied();
+    bool get_isVIP();
+    
+private:
+    //All car variable declarations
+    string color;
+    string make;
+    string model;
+    string lpNum;
+    int floorNum;
+    int spaceNum;
+    int timePeriod;
+    double moneyOwed;
+    bool isOccupied;
+    bool isVIP;
+    
 };
 
 
-#endif
-
+#endif //
